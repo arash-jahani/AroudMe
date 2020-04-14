ef882b4492e7e5a66e3c2f8847f6345e045929c8
arash-jahani <>
2020-04-14 23:43:17 +0430

    Created Application Class

---
+++ app/src/main/java/ir/arashjahani/nearplaces/MyApplication.kt
@@ -0,0 +1,15 @@
+package ir.arashjahani.nearplaces
+
+import android.app.Application
+
+/**
+ * Created By ArashJahani on 2020/04/14
+ */
+class MyApplication:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
