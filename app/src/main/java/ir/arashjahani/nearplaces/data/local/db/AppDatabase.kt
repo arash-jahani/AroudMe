7f87e3d635b6a2f60266fa17b8e154f9fe4c4aea
arash-jahani <>
2020-04-16 18:27:58 +0430

    Created the Database class

---
+++ app/src/main/java/ir/arashjahani/nearplaces/data/local/db/AppDatabase.kt
@@ -0,0 +1,17 @@
+package ir.arashjahani.nearplaces.data.local.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ir.arashjahani.nearplaces.data.model.CategoriesItem
+import ir.arashjahani.nearplaces.data.model.Venue
+
+/**
+ * Created By ArashJahani on 2020/04/16
+ */
+
+@Database(entities = arrayOf(Venue::class,CategoriesItem::class),version = 1)
+abstract class AppDatabase:RoomDatabase() {
+
+
+
+}
